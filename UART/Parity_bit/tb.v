39d06a999a11d35ff98a3f9d4f0e500a2824dc8a
15anuddk <anudskamble03@gmail.com>
2025-07-16 15:47:34 +0530

    Updated

--- UART/Parity_bit/tb.v
+++ UART/Parity_bit/tb.v
@@ -2,20 +2,18 @@
 module tb();
 reg reset;
 reg [7:0] data_in;
-reg [1:0] parity_type;
 
 wire parity_bit;
 
 parity uut(.reset(reset),
             .data_in(data_in),
-            .parity_type(parity_type),
             .parity_bit(parity_bit));
 
 initial begin
   $dumpfile("test.vcd");
   $dumpvars(0,tb);
 
-  $monitor("data_in = %b, paritytype %b , parity %b", data_in,parity_type, parity_bit);
+  $monitor("data_in = %b, parity %b", data_in,parity_bit);
 end
 
 initial
@@ -27,7 +25,7 @@ end
 //  Test
 initial
 begin
-        data_in = 8'b00010111;
+    #10    data_in = 8'b00110111;
     #10 data_in = 8'b00001111;
     #10 data_in = 8'b10101111;
     #10 data_in= 8'b10101001;
@@ -35,13 +33,4 @@ begin
     #10 data_in = 8'b10111101;
 end
 
-//  Parity Types
-initial
-begin
-        parity_type = 2'b00;
-    #10 parity_type = 2'b00;
-    #10 parity_type = 2'b01;
-    #10 parity_type = 2'b10;
-    #10 parity_type = 2'b11;
-end
 endmodule
